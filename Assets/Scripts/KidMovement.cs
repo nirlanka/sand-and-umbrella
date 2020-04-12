e5989d762450d665167f78273cdc5b6d74086d6f
Nir Lanka <7931869+nirlanka@users.noreply.github.com>
2020-04-12 18:34:26 +0800

    Implement rotation, add tree branch

--- Assets/Scripts/KidMovement.cs
+++ Assets/Scripts/KidMovement.cs
@@ -15,17 +15,8 @@ public class KidMovement : MonoBehaviour
 
     void Update()
     {
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-
-        transform.Translate(x, 0, y);
-
-        if (Input.GetButtonDown("Jump") && isOnGround)
-        {
-            var jumpVector = new Vector3(0, jump, 0);
-            _rigidbody.AddForce(jumpVector, ForceMode.Impulse);
-            isOnGround = false;
-        }
+        Move();
+        Jump();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -33,6 +24,44 @@ public class KidMovement : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             isOnGround = true;
+        }
+    }
+
+    void Move()
+    {
+        // NOTE:
+        // GetAxis --> smoothened
+        // GetAxisRaw --> -1, 0, 1
+
+        //var xInp = Input.GetAxisRaw("Horizontal");
+        var xInp = Input.GetAxis("Horizontal");
+        //var yInp = Input.GetAxisRaw("Vertical");
+        var yInp = Input.GetAxis("Vertical");
+
+        if (xInp != 0 || yInp != 0)
+        {
+            //var xMove = xInp * Time.deltaTime * speed;
+            //var yMove = yInp * Time.deltaTime * speed;
+
+            //var movement = new Vector3(xInp, 0, yInp);
+            var movement = new Vector3(xInp, 0, yInp) * speed * Time.deltaTime;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+            //transform.rotation = Quaternion.LookRotation(movement);
+
+            //transform.Translate(xMove, 0, yMove);
+            //transform.Translate(movement * speed * Time.deltaTime, Space.World);
+            transform.Translate(movement, Space.World);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isOnGround)
+        {
+            var jumpVector = new Vector3(0, jump, 0);
+            _rigidbody.AddForce(jumpVector, ForceMode.Impulse);
+            isOnGround = false;
         }
     }
 }
