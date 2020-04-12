3b0476992639e0cf33a23b4a05575c6fb864d440
Nir Lanka <7931869+nirlanka@users.noreply.github.com>
2020-04-12 14:19:18 +0800

    Create mini island, moving player

---
+++ Assets/Scripts/KidMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class KidMovement : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+
+    public float speed = 10.0f;
+    public float jump = 5.0f;
+    public bool isOnGround = true;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(x, 0, y);
+
+        if (Input.GetButtonDown("Jump") && isOnGround)
+        {
+            var jumpVector = new Vector3(0, jump, 0);
+            _rigidbody.AddForce(jumpVector, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isOnGround = true;
+        }
+    }
+}
